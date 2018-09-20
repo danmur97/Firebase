d1d4120d8d27c45ee4096bd5a10054a7f7715cd7
danmur97 <danmur97@outlook.com>
2018-09-20 11:47:48 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyC2uDJ8MA0x-BdS_b2VdhpGJJWGm-uC0iQ",
+    authDomain: "rutas-abd8d.firebaseapp.com",
+    databaseURL: "https://rutas-abd8d.firebaseio.com",
+    projectId: "rutas-abd8d",
+    storageBucket: "rutas-abd8d.appspot.com",
+    messagingSenderId: "1056497253760"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +40,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: Observable<any[]>;
+  constructor(public navCtrl: NavController,afDB: AngularFireDatabase) {
+    this.items = afDB.list('tareas').valueChanges();
   }
 
 }
